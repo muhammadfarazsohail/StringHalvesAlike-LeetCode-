5e1a5cf68d167c0af2adb2d423378b26742c92d5
muhammadfarazsohail <mfg_so115@hotmail.com>
2022-11-30 21:29:37 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿namespace StringHalvesAlike_LeetCode_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Program program= new Program();
+            var x = program.HalvesAreAlike("faaz");
+            Console.WriteLine(x);
+        }
+
+        public bool HalvesAreAlike(string s)
+        {
+            var strLen = s.Length;
+            var stringHalf = strLen / 2;
+            var str1 = 0;
+            var str2=0;
+
+            for (int i = 0; i < stringHalf; i++)
+            {
+                var charInStr= s[i].ToString().ToLower();
+                if (charInStr == "a"|| charInStr == "e" || charInStr == "i" || charInStr == "o" || charInStr == "u")
+                {
+                    str1++;
+                }
+            }
+
+            for (int i = stringHalf; i < strLen; i++)
+            {
+                var charInStr = s[i].ToString().ToLower();
+                if (charInStr == "a" || charInStr == "e" || charInStr == "i" || charInStr == "o" || charInStr == "u")
+                {
+                    str2++;
+                }
+            }
+
+            if (str1 == str2)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
